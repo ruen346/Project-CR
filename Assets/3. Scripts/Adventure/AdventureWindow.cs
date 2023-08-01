71b8b84933098accc3d533d84fb6d26f529da7a3
ruen <ruen346@gmail.com>
2023-08-01 20:46:34 +0900

    프린세스 스테이지 준비 화면 제작

--- Assets/3. Scripts/Adventure/AdventureWindow.cs
+++ Assets/3. Scripts/Adventure/AdventureWindow.cs
@@ -1,4 +1,17 @@
 public class AdventureWindow : BaseWindow
 {
-
+    public void OnClickPrincessStage()
+    {
+        MenuManager.Instance.OpenWindow("PrincessReadyWindow");
+    }
+    
+    public void OnClickArchiveStage()
+    {
+        
+    }
+    
+    public void OnClickStarStage()
+    {
+        
+    }
 }
