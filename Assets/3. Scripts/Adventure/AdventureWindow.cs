3d1787ff722764bd599777f8e5c2fa2e8a7b832c
ruen <ruen346@gmail.com>
2023-08-18 21:53:58 +0900

    스테이지 오픈 예정 팝업 추가

--- Assets/3. Scripts/Adventure/AdventureWindow.cs
+++ Assets/3. Scripts/Adventure/AdventureWindow.cs
@@ -7,11 +7,11 @@ public class AdventureWindow : BaseWindow
     
     public void OnClickArchiveStage()
     {
-        
+        MenuManager.Instance.OpenMessageWindow("아카이브 스테이지", "추후 오픈될 스테이지 입니다.\n다음을 기대해 주세요", true);
     }
     
     public void OnClickStarStage()
     {
-        
+        MenuManager.Instance.OpenMessageWindow("스타 스테이지", "추후 오픈될 스테이지 입니다.\n다음을 기대해 주세요", true);
     }
 }
