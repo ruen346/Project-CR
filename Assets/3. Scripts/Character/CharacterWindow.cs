a687de31ce13e47a3ef6d1d203b553d08aac20d4
ruen <ruen346@gmail.com>
2023-08-19 18:06:33 +0900

    캐릭터 팝업 스크롤 기능 추가

--- Assets/3. Scripts/Character/CharacterWindow.cs
+++ Assets/3. Scripts/Character/CharacterWindow.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class CharacterWindow : BaseWindow
 {
     public GameObject characterIcon;
-    public GameObject viewport;
+    public GameObject characterParent;
     
     public void Start()
     {
         foreach (var data in DataManager.Instance.characterData.CharacterInfoDatas)
         {
-            var character = Instantiate(characterIcon, viewport.transform);
+            var character = Instantiate(characterIcon, characterParent.transform);
             character.GetComponent<CharacterIcon>().Init(data);
         }
     }
