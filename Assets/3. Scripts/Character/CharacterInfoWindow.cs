95f004ce24b1d2f308b87d1fa6325238a40e8ab2
ruen <ruen346@gmail.com>
2023-08-20 00:36:22 +0900

    캐릭터 상세 정보창 기능 구현

--- Assets/3. Scripts/Character/CharacterInfoWindow.cs
+++ Assets/3. Scripts/Character/CharacterInfoWindow.cs
@@ -1,6 +1,49 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 public class CharacterInfoWindow : BaseWindow
 {
+    public Image characterImage;
+    public List<Image> starImages;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI levelText;
+    public TextMeshProUGUI damageText;
+    public TextMeshProUGUI hpText;
+
+    private CharacterInfoData characterData;
+
     public void Init(CharacterInfoData data)
+    {
+        characterData = data;
+
+        SetImages();
+        SetTexts();
+    }
+
+    private void SetImages()
+    {
+        characterImage.sprite = Resources.Load<Sprite>($"Texture/Character/Image/Character{characterData.id}");
+        for (int i = 3; i > 0; i--)
+        {
+            if (characterData.star < i)
+            {
+                starImages[i - 1].color = Color.gray; 
+            }
+        }
+    }
+
+    private void SetTexts()
+    {
+        nameText.text = characterData.name;
+        levelText.text = $"Lv.{characterData.level}";
+        damageText.text = $"공격력 : {characterData.damage}";
+        hpText.text = $"채력 : {characterData.hp}";
+    }
+
+    public void OnClickUpgradeButton()
     {
         
     }
