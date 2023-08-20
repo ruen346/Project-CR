35b7e7f936e14b3eb98f8aa16b8ff5851347d11a
ruen <ruen346@gmail.com>
2023-08-20 21:08:09 +0900

    홈 화면 터치 대화 기능 추가

--- Assets/3. Scripts/Manager/BackgroundManager.cs
+++ Assets/3. Scripts/Manager/BackgroundManager.cs
@@ -1,9 +1,13 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BackgroundManager : MonoSingleton<BackgroundManager>
 {
     public Image image;
+    public GameObject dialogObject;
+
+    private bool isDialog;
     
     private void Start()
     {
@@ -13,5 +17,24 @@ public class BackgroundManager : MonoSingleton<BackgroundManager>
     public void SetBackground(int id)
     {
         image.sprite = Resources.Load<Sprite>($"Texture/Character/Image/Character{id}");
+    }
+
+    public void PlayDialog()
+    {
+        if (!isDialog)
+        {
+            StartCoroutine(DoDialog());
+        }
+    }
+
+    private IEnumerator DoDialog()
+    {
+        dialogObject.SetActive(true);
+        isDialog = true;
+
+        yield return new WaitForSeconds(3f);
+        
+        dialogObject.SetActive(false);
+        isDialog = false;
     }
 }
